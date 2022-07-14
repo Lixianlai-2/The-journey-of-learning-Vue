753d3aad8288c7e98d37df0d649e8c1a54cfd91e
Li <1643565650@qq.com>
2022-07-14 09:55:52 +0800

    前置跟后置全局路由守卫

--- afterVueCli/Vue_Test/src/router/index.js
+++ afterVueCli/Vue_Test/src/router/index.js
@@ -8,20 +8,38 @@ import Detail from "../pages/Detail.vue";
 const router = new VueRouter({
   routes: [
     {
-      // name: "guanyu",
+      name: "guanyu",
       path: "/about",
       component: About,
+      meta: {
+        title: "关于",
+      },
     },
     {
+      name: "zhuye",
       path: "/home",
       component: Home,
+      meta: {
+        title: "主页",
+      },
       children: [
         {
+          name: "xinwen",
           path: "news",
+          meta: {
+            // 需要鉴权（鉴定是否有权限）
+            isAuth: true,
+            title: "新闻",
+          },
           component: News,
         },
         {
+          name: "xiaoxi",
           path: "message",
+          meta: {
+            isAuth: true,
+            title: "消息",
+          },
           component: Message,
           children: [
             {
@@ -45,6 +63,27 @@ const router = new VueRouter({
       ],
     },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  console.log(to, from);
+
+  if (to.meta.isAuth) {
+    // 判断当前路由是否需要进行权限控制
+    if (localStorage.school === "qinghua") {
+      // document.title = to.meta.title || "Vue学习";
+      next();
+    } else {
+      alert("学校不对，无法读取");
+    }
+  } else {
+    // document.title = to.meta.title || "Vue学习";
+    next();
+  }
+});
+
+router.afterEach((to) => {
+  document.title = to.meta.title || "Vue学习";
 });
 
 export default router;
