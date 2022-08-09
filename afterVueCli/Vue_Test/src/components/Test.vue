8433755057e5fb6d13ef798c738fe8510365246c
Li <1643565650@qq.com>
2022-08-09 19:04:31 +0800

    把vue3部分转移到这里

---
+++ afterVueCli/Vue_Test/src/components/Test.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <button @click="isShow = !isShow">显示/隐藏</button>
+    <transition name="hello" appear>
+      <h1 v-show="isShow">你好</h1>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isShow: true,
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  background: orange;
+}
+
+.hello.enter.to,
+.hello.enter.leave {
+}
+
+/* .hello-enter-active {
+  animation: move 1s;
+}
+
+.hello-leave-active {
+  animation: move 1s reverse;
+}
+
+@keyframes move {
+  from {
+    transform: translateX(-100%);
+  }
+  to {
+    transform: translateX(0px);
+  }
+} */
+</style>
